b58558d5b25fcea4543632fe4875e6e92d78d50e
Dwi Zulyan Hamid <pokokseneng.12@gmail.com>
2021-07-30 02:17:01 +0700

    add more feature and fix the navigation on windows or tablet

--- docs/app.js
+++ docs/app.js
@@ -1,18 +1,31 @@
 function clickFunction(){
 	var button = document.getElementById("bars");
 	var link = document.getElementById("link");
-
-	if(link.style.marginLeft === '-200px'){
-		link.style.marginLeft = '-20px'
+	console.log('success!!')
+	if(link.style.left === '-70%'){
+		link.style.left = '-10%'
 	}
 	else{
-		link.style.marginLeft = '-200px'
+		link.style.left = '-70%'
 	}
 	
 }
 var clickLink = document.querySelectorAll(".c");
 for( var click of clickLink){
 	click.addEventListener('click',function(){
-		link.style.marginLeft = '-200px'
+		link.style.left = '-70%'
 	})
 }
+
+var defValue = 0;
+var text = 'Hiiii, Saya Dwi Zulyan';
+var speed = 300;
+
+function typeWriter(){
+	if(defValue < text.length){
+		document.getElementById("typewrite").innerHTML += text.charAt(defValue);
+		defValue++;
+		setTimeout(typeWriter, speed)
+	}
+}
+
