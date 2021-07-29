912bd24ee515276b1247220589bb7e0fcefde464
Dwi Zulyan Hamid <pokokseneng.12@gmail.com>
2021-07-29 21:57:19 +0700

    mengubah tampilan navigasi

--- docs/app.js
+++ docs/app.js
@@ -1,9 +1,18 @@
 function clickFunction(){
+	var button = document.getElementById("bars");
 	var link = document.getElementById("link");
 
-	if(link.style.display === "flex"){
-		link.style.display = "none";
-	}else{
-		link.style.display = "flex";
+	if(link.style.marginLeft === '-200px'){
+		link.style.marginLeft = '-20px'
 	}
+	else{
+		link.style.marginLeft = '-200px'
+	}
+	
+}
+var clickLink = document.querySelectorAll(".c");
+for( var click of clickLink){
+	click.addEventListener('click',function(){
+		link.style.marginLeft = '-200px'
+	})
 }
